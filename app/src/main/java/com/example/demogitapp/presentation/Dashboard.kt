918f5ff02163c71cba2a7d33beaa1b5b0aca2076
Ayan Debnath <rintu98deb@gmail.com>
2023-09-15 08:42:37 +0530

    start implementating screen dashboard

---
+++ app/src/main/java/com/example/demogitapp/presentation/Dashboard.kt
@@ -0,0 +1,12 @@
+package com.example.demogitapp.presentation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Dashboard() {
+    Column {
+        Text(text = "Dashboard Screen")
+    }
+}
